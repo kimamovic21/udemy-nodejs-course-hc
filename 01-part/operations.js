9552d8dd0cb111de0e212f46d25fcfe1521f9c5b
Kerim Imamović <imamovic.kerim@gmail.com>
2025-09-15 17:28:05 +0200

    Operations in Javascript

---
+++ 01-part/operations.js
@@ -0,0 +1,31 @@
+let score = 102;
+let bonus = 25;
+
+let totalScore = score + bonus;
+console.log(totalScore);
+
+let addition = 4 + 5;
+let subtraction = 9 - 3;
+let multiplication = 3 * 5;
+let division = 8 / 2;
+let remainder = 9 % 2;
+let exponent = 2 ** 3;
+console.log(exponent);
+
+let myScore = 110;
+myScore++;
+console.log(myScore);
+
+let credits = 56;
+credits--
+console.log(credits);
+
+// Comparison operation
+let num1 = 3
+let num2 = 3;
+let num3 = 4;
+
+console.log(num1 == num2);
+console.log(num1 != num3);
+console.log(num1 > num3);
+console.log(num1 < num3);
