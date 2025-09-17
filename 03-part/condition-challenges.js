7dee61833106eabb8cfa5f351115d7833ac4632c
Kerim Imamović <imamovic.kerim@gmail.com>
2025-09-17 15:30:02 +0200

    Five challenges on conditions in Javascript

---
+++ 03-part/condition-challenges.js
@@ -0,0 +1,50 @@
+let num1 = 2;
+let num2 = 3;
+
+// console.log('I am regular upper code');
+
+// if (num1 > num2) {
+//   console.log('Num 1 is greater than num2');
+// } else {
+//   console.log('Nope, num1 is not greater');
+// };
+
+// console.log('I am regular bottom code');
+
+
+let username = 'imamovickerim';
+let anotherUsername = 'imamovickerim';
+
+// if (username == anotherUsername) {
+//   console.log('Pick another username');
+// } else {
+//   console.log('You can pick this username');
+// };
+
+
+let score = 44;
+
+// if (typeof score === 'number') {
+//   console.log('Yep, this is a number');
+// } else {
+//   console.log('No that is not a number');
+// };
+
+
+let isTeaReady = false;
+
+// if (isTeaReady) {
+//   console.log('Tea is ready');
+// } else {
+//   console.log('Tea is not ready');
+// };
+
+
+let items = ['item1', 'item2'];
+// console.log(items.length);
+
+// if (items.length === 0) {
+//   console.log('Array is empty');
+// } else {
+//   console.log('Array is not empty');
+// };
