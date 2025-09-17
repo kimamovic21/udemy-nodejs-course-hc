cc182cf0ac6d23cd06742d91156a39ce82646e68
Kerim Imamović <imamovic.kerim@gmail.com>
2025-09-17 11:26:50 +0200

    Primitives in Javascript

---
+++ 02-part/primitives.js
@@ -0,0 +1,45 @@
+// Number
+
+let balance = 120;
+let anotherBalance = new Number(120);
+
+// console.log(balance);
+// console.log(anotherBalance);
+
+// console.log(typeof balance);
+// console.log(typeof anotherBalance);
+
+
+// Boolean
+let isActive = true;
+let isReallyActive = new Boolean(true);
+
+// console.log(isActive);
+// console.log(isReallyActive);
+
+
+// Null and undefined
+let firstName
+let lastName = null
+// console.log(firstName);
+// console.log(lastName);
+
+
+// String
+let greet = 'Hello';
+let username = 'Kerim';
+let result = `Value is ${2 * 2}`;
+
+let greetMessage = `${greet} ${username} !`;
+// console.log(typeof greetMessage);
+// console.log(greetMessage);
+// console.log(result);
+
+
+
+let symbol1 = Symbol('kerim');
+let symbol2 = Symbol('kerim');
+// console.log(symbol1);
+// console.log(symbol2);
+// console.log(symbol1 == symbol2);
+// console.log(symbol1 === symbol2);
