dc2bfafabe996ba4ec53ef764ae04100e8b6b142
Kerim Imamović <imamovic.kerim@gmail.com>
2025-09-18 19:01:20 +0200

    Using the FS module to read and write files

--- 01-modules/index.js
+++ 01-modules/index.js
@@ -1,5 +1,11 @@
-const fs = require('fs');
+const fs = require('node:fs');
 
 const content = fs.readFileSync('notes.txt', 'utf-8');
 
 console.log(content);
+
+fs.writeFileSync('copy.txt', content, 'utf-8');
+fs.appendFileSync('copy.txt', `\n${content}`, 'utf-8');
+fs.mkdirSync('games', { recursive: true });
+// fs.rmdirSync('games/myGames');
+// fs.unlinkSync('copy.txt')
