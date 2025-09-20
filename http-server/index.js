7169419711dbc7e98e4779270f12ac58406a7258
Kerim Imamović <imamovic.kerim@gmail.com>
2025-09-20 14:55:05 +0200

    Building a custom server with Node.js

--- http-server/index.js
+++ http-server/index.js
@@ -3,10 +3,26 @@ const http = require('node:http');
 const PORT = 8000;
 
 const server = http.createServer(function (req, res) {
-  console.log('I got an incoming request');
+  console.log(`Incoming request at: [${Date.now()}]`);
+  console.log('Request url: ', req.url);
 
-  res.writeHead(200);
-  res.end('Thanks for visiting my server');
+  switch (req.url) {
+    case '/':
+      res.writeHead(200);
+      return res.end(`Homepage`);
+
+    case '/contact-us':
+      res.writeHead(200);
+      return res.end(`Contact me at: kimamovic21@email.com`);
+
+    case '/about':
+      res.writeHead(200);
+      return res.end(`I am a developer`);
+
+    default:
+      res.writeHead(404);
+      res.end(`You are lost...`);
+  };
 });
 
 server.listen(PORT, function () {
