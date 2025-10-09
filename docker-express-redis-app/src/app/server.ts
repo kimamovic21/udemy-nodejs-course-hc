9e774fb2c61787c451759588e3160a28c51a7bd3
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-09 15:37:35 +0200

    Introduction to Docker compose

---
+++ docker-express-redis-app/src/app/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const PORT = process.env.PORT ?? 8000;
+
+const app = express();
+
+app.get('/', (_req, res) => {
+  res.json({ message: 'Hello from Docker Express Redis App!' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+export default app;
---
+++ docker-express-redis-app/src/index.ts
@@ -0,0 +1,48 @@
+import { Redis } from 'ioredis';
+import http from 'http';
+import pg from 'pg';
+import app from './app/server';
+
+async function init() {
+  try {
+    console.log(`Connecting Redis...`);
+    const redis = new Redis('redis://localhost:6379', { lazyConnect: true });
+    await redis.connect();
+    console.log(`Redis connection success...`);
+
+    console.log(`Connecting Postgres...`);
+    const { Client } = pg;
+    const client = new Client({
+      host: 'localhost',
+      port: 5431,
+      database: 'postgres',
+      user: 'postgres',
+      password: 'postgres',
+    });
+    await client.connect();
+    console.log(`Postgres connection success...`);
+
+    const PORT = process.env.PORT ?? 8000;
+    const server = http.createServer(app);
+
+    server.on('error', (err: NodeJS.ErrnoException) => {
+      if (err.code === 'EADDRINUSE') {
+        console.error(`Port ${PORT} is already in use. Please choose another port.`);
+        process.exit(1);
+      } else {
+        console.error(`Server error: ${err}`);
+        process.exit(1);
+      }
+    });
+
+    server.listen(PORT, () => {
+      console.log(`HTTP server is listening on PORT: ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error(`Error starting server: ${err}`);
+    process.exit(1);
+  }
+};
+
+init();
