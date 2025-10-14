b7c1abef52fde5171dc2afbadf9c8cc3192ba862
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-14 12:34:01 +0200

    Separation of app with index

--- project-management/src/index.ts
+++ project-management/src/index.ts
@@ -1,12 +1,10 @@
-import express, { type Request, type Response } from 'express';
+import { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
+import app from './app';
 
 dotenv.config();
 
-const app = express();
 const PORT = process.env.PORT || 8000;
-
-app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: 'Hello from Project Management API!' });
