e825a0861cf011b7ac25e80d79dc58cb168a768c
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-16 10:46:48 +0200

    How to send emails in Node.js

--- project-management/src/utils/mail.ts
+++ project-management/src/utils/mail.ts
@@ -1,4 +1,41 @@
 import Mailgen from 'mailgen';
+import nodemailer from 'nodemailer';
+
+export async function sendEmail(options) {
+  const mailGenerator = new Mailgen({
+    theme: 'default',
+    product: {
+      name: 'Task Manager',
+      link: 'https://taskmanagerlink.com',
+    },
+  });
+
+  const emailTextual = mailGenerator.generatePlaintext(options.mailgenContent);
+  const emailHtml = mailGenerator.generate(options.mailgenContent);
+
+  const transporter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS
+    },
+  });
+
+  const mail = {
+    from: 'mail.taskmanager@example.com',
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHtml,
+  };
+
+  try {
+    await transporter.sendMail(mail);
+  } catch (err) {
+    console.error(`Email service failed: ${err}`);
+  };
+};
 
 export function emailVerificationMailgenContent(
   username: string,
