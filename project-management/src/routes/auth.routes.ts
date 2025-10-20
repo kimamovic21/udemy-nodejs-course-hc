a1e404dd203a17a01f2e4acdab29efd7c7695558
Kerim Imamović <imamovic.kerim@gmail.com>
2025-10-20 14:08:41 +0200

    All routes and validators of auth

--- project-management/src/routes/auth.routes.ts
+++ project-management/src/routes/auth.routes.ts
@@ -3,16 +3,27 @@ import { validate } from '../middlewares/validator.middleware';
 import { verifyJWT } from '../middlewares/auth.middleware';
 import {
   userRegisterValidator,
-  userLoginValidator
+  userLoginValidator,
+  userForgotPasswordValidator,
+  userResetForgotPasswordValidator,
+  userChangeCurrentPasswordValidator
 } from '../validators/index';
 import {
   registerUser,
   loginUser,
-  logoutUser
+  logoutUser,
+  verifyEmail,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  resetForgotPassword,
+  getCurrentUser,
+  changeCurrentPassword,
+  resendEmailVerification
 } from '../controllers/auth.controller';
 
 const router = Router();
 
+// unsecured routes
 router
   .route('/register')
   .post(userRegisterValidator(), validate, registerUser);
@@ -20,7 +31,30 @@ router
   .route('/login')
   .post(userLoginValidator(), validate, loginUser);
 router
+  .route('/verify-email/:verificationToken')
+  .get(verifyEmail);
+router
+  .route('/refresh-token')
+  .post(refreshAccessToken)
+router
+  .route('/forgot-password')
+  .post(userForgotPasswordValidator(), validate, forgotPasswordRequest)
+router
+  .route('/reset-password/:resetToken')
+  .post(userResetForgotPasswordValidator, validate, resetForgotPassword)
+
+// secured routes
+router
   .route('/logout')
   .post(verifyJWT, logoutUser);
+router
+  .route('/current-user')
+  .post(verifyJWT, getCurrentUser);
+router
+  .route('/change-password')
+  .post(verifyJWT, userChangeCurrentPasswordValidator(), validate, changeCurrentPassword);
+router
+  .route('/resend-email-verification')
+  .post(verifyJWT, resendEmailVerification);
 
 export default router;
--- project-management/src/validators/index.ts
+++ project-management/src/validators/index.ts
@@ -37,3 +37,32 @@ export function userLoginValidator() {
       .withMessage('Password is required!')
   ];
 };
+
+export function userChangeCurrentPasswordValidator() {
+  return [
+    body('oldPassword')
+      .notEmpty()
+      .withMessage('Old password is required!'),
+    body('newPassword')
+      .notEmpty()
+      .withMessage('New password is required!')
+  ];
+};
+
+export function userForgotPasswordValidator() {
+  return [
+    body('email')
+      .notEmpty()
+      .withMessage('Email is required!')
+      .isEmail()
+      .withMessage('Email is invalid!')
+  ];
+};
+
+export function userResetForgotPasswordValidator() {
+  return [
+    body('newPassword')
+      .notEmpty()
+      .withMessage('Password is required!')
+  ];
+};
